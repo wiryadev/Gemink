0c00d5a387d5ca5608a2c1b618c5a2bbdd710bd7
wiryadev <abrarwiryawan@gmail.com>
2023-05-20 14:27:59 +0700

    Initial Commit

---
+++ Gemink/GeminkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminkApp.swift
+//  Gemink
+//
+//  Created by Abrar Wiryawan on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GeminkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
